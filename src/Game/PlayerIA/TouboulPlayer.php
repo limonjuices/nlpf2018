716421df4916453ed4003b2a491f9e2a8a0722f1
limonjuices <zaccharie.touboul@epita.fr>
2017-11-08 21:47:12 +0100

    Update TouboulPlayer.php

--- src/Game/PlayerIA/TouboulPlayer.php
+++ src/Game/PlayerIA/TouboulPlayer.php
@@ -40,7 +40,7 @@ class TouboulPlayer extends Player
         // -------------------------------------    -----------------------------------------------------
         // How can i display the result of each round ? $this->prettyDisplay()
         // -------------------------------------    -----------------------------------------------------
-   $round = $this->result->getNbRound();
+        $round = $this->result->getNbRound();
         $choice = parent::scissorsChoice();
         
         if ($this->result->getLastChoiceFor($this->mySide) === 'scissors' && $this->result->getLastScoreFor($this->mySide) === 0)
@@ -50,33 +50,21 @@ class TouboulPlayer extends Player
         if ($this->result->getLastChoiceFor($this->mySide) === 'scissors' && $this->result->getLastScoreFor($this->mySide) === 1)
             $choice = parent::scissorsChoice();
 
-        if ( $round > 500 )
-        {
 
-          if ($this->result->getLastScoreFor($this->opponentSide) === 5 && $this->result->getChoicesFor($this->opponentSide) === 'rock')
-              $choice = parent::paperChoice();
-          if ($this->result->getLastScoreFor($this->opponentSide) === 5 && $this->result->getChoicesFor($this->opponentSide) === 'paper')
-              $choice = parent::scissorsChoice();
-          if ($this->result->getLastScoreFor($this->opponentSide) === 5 && $this->result->getChoicesFor($this->opponentSide) === 'scissors')
-              $choice = parent::rockChoice();
-          if ($this->result->getLastChoiceFor($this->mySide) === 'rock' && $this->result->getLastScoreFor($this->mySide) === 0)
-              $choice = parent::paperChoice();
-          if ($this->result->getLastChoiceFor($this->mySide) === 'paper' && $this->result->getLastScoreFor($this->mySide) === 0)
-              $choice = parent::scissorsChoice();
-          if ($this->result->getLastChoiceFor($this->mySide) === 'scissors' && $this->result->getLastScoreFor($this->mySide) === 0)
-              $choice = parent::rockChoice();
-        }
-        else
-        {
-            $r = mt_rand(1,3);
-            if ( $r == 1)
-              $choice = parent::rockChoice();
-            if ( $r == 2 )
-              $choice = parent::paperChoice();
-            if ( $r == 3 )
-              $choice = parent::scissorsChoice();
-        }
 
+        if ($this->result->getLastChoiceFor($this->mySide) === 'rock' && $this->result->getLastScoreFor($this->mySide) === 0)
+            $choice = parent::paperChoice();
+        if ($this->result->getLastChoiceFor($this->mySide) === 'rock' && $this->result->getLastScoreFor($this->mySide) === 5)
+            $choice = parent::scissorsChoice();
+        if ($this->result->getLastChoiceFor($this->mySide) === 'rock' && $this->result->getLastScoreFor($this->mySide) === 1)
+            $choice = parent::rockChoice();
+
+        if ($this->result->getLastChoiceFor($this->mySide) === 'paper' && $this->result->getLastScoreFor($this->mySide) === 0)
+            $choice = parent::scissorsChoice();
+        if ($this->result->getLastChoiceFor($this->mySide) === 'paper' && $this->result->getLastScoreFor($this->mySide) === 5)
+            $choice = parent::rockChoice();
+        if ($this->result->getLastChoiceFor($this->mySide) === 'paper' && $this->result->getLastScoreFor($this->mySide) === 1)
+            $choice = parent::paperChoice();
         return $choice;
     }
 };
