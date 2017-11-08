7580e84b39f10e2d4e0ba7f97f2fcee1d7f64c19
limonjuices <zaccharie.touboul@epita.fr>
2017-11-08 21:58:52 +0100

    Update TouboulPlayer.php

--- src/Game/PlayerIA/TouboulPlayer.php
+++ src/Game/PlayerIA/TouboulPlayer.php
@@ -17,7 +17,6 @@ class TouboulPlayer extends Player
 
     public function getChoice()
     {
-        
         $round = $this->result->getNbRound();
         $choice = parent::scissorsChoice();
         
@@ -43,6 +42,34 @@ class TouboulPlayer extends Player
             $choice = parent::rockChoice();
         if ($this->result->getLastChoiceFor($this->mySide) === 'paper' && $this->result->getLastScoreFor($this->mySide) === 1)
             $choice = parent::paperChoice();
+
+        if ( $round > 10 )
+        {
+
+          if ($this->result->getLastScoreFor($this->opponentSide) === 5 && $this->result->getChoicesFor($this->opponentSide) === 'rock')
+              $choice = parent::paperChoice();
+          if ($this->result->getLastScoreFor($this->opponentSide) === 5 && $this->result->getChoicesFor($this->opponentSide) === 'paper')
+              $choice = parent::scissorsChoice();
+          if ($this->result->getLastScoreFor($this->opponentSide) === 5 && $this->result->getChoicesFor($this->opponentSide) === 'scissors')
+              $choice = parent::rockChoice();
+          if ($this->result->getLastChoiceFor($this->mySide) === 'rock' && $this->result->getLastScoreFor($this->mySide) === 0)
+              $choice = parent::paperChoice();
+          if ($this->result->getLastChoiceFor($this->mySide) === 'paper' && $this->result->getLastScoreFor($this->mySide) === 0)
+              $choice = parent::scissorsChoice();
+          if ($this->result->getLastChoiceFor($this->mySide) === 'scissors' && $this->result->getLastScoreFor($this->mySide) === 0)
+              $choice = parent::rockChoice();
+        }
+        else
+        {
+            $r = mt_rand(1,3);
+            if ( $r == 1)
+              $choice = parent::rockChoice();
+            if ( $r == 2 )
+              $choice = parent::paperChoice();
+            if ( $r == 3 )
+              $choice = parent::scissorsChoice();
+        }
+
         return $choice;
     }
 };
